f4f42c7db3375efd2132f8bf6cb0ad44ce3c998b
Denperidge <denperidge@gmail.com>
2022-07-31 22:08:35 +0200

    Reworked Project constructor

--- app/index.ts
+++ app/index.ts
@@ -1,9 +1,15 @@
+import twentytwo from './data/2022.json';
 
 // Globals
 let partners : {[slug: string]: Partner;} = {};
 let students : {[slug: string]: Partner;} = {};
 let projects : {[id: string]: Partner;} = {};
 
+// Interface of Team object found in data
+interface Team {
+    students: Array<string>
+    coaches: Array<string>
+}
 
 // Classes
 class Project {
@@ -11,61 +17,48 @@ class Project {
     name: string;
     description: string;
     logo: URL;
-    private _teamId: string;
+    team: Team;
     repository: URL;
     website?: URL;
     partners: Array<Partner>;
 
-    //constructor(id : string, name: string, )
     // Contsructor based off the json impelementation for the website
-    //constructor(id : string, name : string, description : string, logo : string, team: Array<string>, repository: string, website?: string);
-    constructor(id : string, name : string, description : string, logo : string, team: string, repository: string|URL, website?: string|URL, partners : Array<string>) {
+    constructor(id : string, name : string, description : string, logo : string, team: Team, repository: string|URL, partners : Array<string>, website?: string|URL) {
         //if (website instanceof ) website = 
         this.id = id;
         this.name = name;
         this.description = description;
         this.logo = new URL(logo);
-        this._teamId = team;
+        this.team = team;
         this.repository = new URL (repository);
         if (website) this.website = new URL(website);
-        this.partners = 
+        this.partners = partners;
     }
-
-    set team(value : string|Team) {
-        if (value instanceof Team) {
-            this._teamId = value.id;
-        } else {
-            this._teamId = value;
-        }
-    }
-
-    get team() : Team {
-        return this.team;
-    }
-
-
 }
+
 
 class Partner {
 
 }
 
+
 class Team {
+    /*
     students: Array<Student>;
     coaches: Array<Coach>;
     id : string;
 
-    
+    */
 }
 
 interface Person {
     name: String;
 }
 
-class Student extends Person {
-
+class Student implements Person {
+    name: String;
 }
 
-class Coach extends Person {
-
+class Coach implements Person {
+    name: String;
 }
